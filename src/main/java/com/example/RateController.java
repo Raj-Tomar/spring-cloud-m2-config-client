d311aa374a81846d0c423f7cf127320a2c5736ee
Raj Tomar <rajkishor.tomar@honeywell.com>
2021-03-22 16:01:01 +0530

    Added client decryption and property Refresh

--- src/main/java/com/example/RateController.java
+++ src/main/java/com/example/RateController.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 	
 	@Value("${rate}")
@@ -16,12 +18,16 @@ public class RateController {
 	
 	@Value("${tollstart}")
 	private String tollStart;
+	
+	@Value("${constring}")
+	private String connectionString;
 
 	@GetMapping("/rate")
 	public String getRate(Model m) {
 		m.addAttribute("rateAmount", rate);
 		m.addAttribute("lanes", laneCount);
 		m.addAttribute("tollStart", tollStart);
+		m.addAttribute("connectionString", connectionString);
 
 		// Name of the View
 		return "rateview";
